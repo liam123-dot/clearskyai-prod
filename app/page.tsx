737aaf547c21e5b22917d711c2fea589266bf4a5
Liam Buchanan <l.buchanan358@gmail.com>
2025-10-30 15:23:35 +0000

    correct redirect

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,16 @@ import { getSignInUrl } from "@workos-inc/authkit-nextjs";
 
 export default async function Home() {
   const { user, slug, isAdmin } = await getAuthSession()
-// 
+  console.log('user', user)
+  console.log('slug', slug)
+  console.log('isAdmin', isAdmin)
+
   if (isAdmin) {
     redirect("/admin")
   } else if (user && slug) {
     redirect(`/${slug}`)
   } else {
-    redirect(await getSignInUrl())
+    redirect(await getSignInUrl({redirectUri: `${process.env.NEXT_PUBLIC_APP_URL}/signedin`}))
   }
 
 }
